4ff7715aaea7c1beaa655720dd629396d6a19b82
Jayna-Menard <31700864+JaynaMMenard@users.noreply.github.com>
2020-01-14 13:57:00 -0500

    response+

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let express = require("express")
 //We are creating an express app
 let app = express();
 
+app.get("/", function(request, response){
+    console.log("got a request of /")
+    response.send("hello world")
+})
+
 let port = 3000
 app.listen(port, function(){
     console.log(`I am listening at port ${port}`)
