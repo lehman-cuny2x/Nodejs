e9a68c56b9577b44a153e9446237efba87795316
Jayna-Menard <31700864+JaynaMMenard@users.noreply.github.com>
2020-01-14 14:02:43 -0500

    404Status+

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get("/", function(request, response){
     response.send("hello world")
 })
 
+//http://localhost:3000/hello gives error
+app.get("*", function(request, response){
+    console.log("got a request of /")
+    response.status(404).send("Page not found")
+})
+
 let port = 3000
 app.listen(port, function(){
     console.log(`I am listening at port ${port}`)
