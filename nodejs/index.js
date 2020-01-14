9eb26cc97085233fe1d4573bff2cd70efca18c16
JaynaMMenard <jaynamenard@gmail.com>
2020-01-14 13:28:18 -0500

    NodemonDownload+

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,2 +1,9 @@
 let a = require('./module.js')
-console.log(a.World(), a.Person('Mary Doe!'))
+//console.log(a.World(), a.Person('Mary Doe!'))
+
+let http = require('http')
+
+http.createServer(function(req,res){
+  res.write("Hello World!")
+  res.end()
+}).listen(8080)
